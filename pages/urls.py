131a02bb9944c94ab86fa4d8ef6947d088d84306
Zeredas <wondighizt2@gmail.com>
2025-09-17 18:15:47 +0800

    Action: Added images and updated urls.py as well as views.py

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('hello/<str:name>/', views.hello, name="name"),
+    path('gallery/', views.gallery, name='gallery'),
 ]
